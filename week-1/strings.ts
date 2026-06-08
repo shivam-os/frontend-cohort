89d891281cd0bd7be70c13e41af6fb745c2765b0
Shivam <shivam@abainfotech.com>
2026-06-08 12:13:55 +0530

    Add string problems

---
+++ week-1/strings.ts
@@ -0,0 +1,49 @@
+/*
+1. Reverse words in a given string
+Problem link: https://www.geeksforgeeks.org/reverse-words-in-a-given-string/
+*/
+
+/*
+2. Longest Common Prefix using Sorting
+Problem link: https://www.geeksforgeeks.org/longest-common-prefix-using-sorting/
+*/
+
+/*
+3. Converting Roman Numerals to Integer
+Problem link: https://www.geeksforgeeks.org/roman-number-to-integer/
+*/
+
+/*
+4. Converting Decimal Number lying between 1 to 3999 to Roman Numerals
+Problem link: https://www.geeksforgeeks.org/converting-decimal-number-lying-between-1-to-3999-to-roman-numerals/
+*/
+
+/*
+5. Check if two given Strings are Isomorphic to each other
+Problem link: https://www.geeksforgeeks.org/check-if-two-given-strings-are-isomorphic-to-each-other/
+*/
+
+/*
+6. Check if given String is Pangram or not
+Problem link: https://www.geeksforgeeks.org/pangram-checking/
+*/
+
+/*
+7. Count Distinct Subsequences
+Problem link: https://www.geeksforgeeks.org/count-distinct-subsequences/
+*/
+
+/*
+8. Program to validate an IP address
+Problem link: https://www.geeksforgeeks.org/program-to-validate-an-ip-address/
+*/
+
+/*
+9. Find largest word in dictionary by deleting some characters of given string
+Problem link: https://www.geeksforgeeks.org/find-largest-word-dictionary-deleting-characters-given-string/
+*/
+
+/*
+10. Multiply Large Numbers represented as Strings
+Problem link: https://www.geeksforgeeks.org/multiply-large-numbers-represented-as-strings/
+*/
