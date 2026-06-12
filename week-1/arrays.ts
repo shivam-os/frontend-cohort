931a370bcb19650bd15f6f316aac316ac1c76f04
Shivam <shivam89raj45@gmail.com>
2026-06-12 23:54:04 +0530

    Update the solutions

--- week-1/arrays.ts
+++ week-1/arrays.ts
@@ -3,16 +3,16 @@
 Problem link: https://www.geeksforgeeks.org/problems/peak-element/1
 */
 const findPeakElement = (arr: number[]) => {
-    const n = arr.length;
+  const n = arr.length;
 
-    if (n === 1 || arr[0] > arr[1]) return 0;
-    if (arr[n - 1] > arr[n - 2]) return n - 1;
+  if (n === 1 || arr[0] > arr[1]) return 0;
+  if (arr[n - 1] > arr[n - 2]) return n - 1;
 
-    for (let i = 1; i < n - 1; i++) {
-        if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) return i;
-    }
+  for (let i = 1; i < n - 1; i++) {
+    if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) return i;
+  }
 
-    return -1;
+  return -1;
 };
 
 /*
@@ -20,14 +20,15 @@ const findPeakElement = (arr: number[]) => {
 Problem link: https://www.geeksforgeeks.org/problems/find-minimum-and-maximum-element-in-an-array4428/1
 */
 const findMinMax = (arr: number[]): [number, number] => {
-    let maxElement = arr[0], minElement = arr[0];
+  let maxElement = arr[0],
+    minElement = arr[0];
 
-    for (let item of arr) {
-        if (item > maxElement) maxElement = item;
-        if (item < minElement) minElement = item;
-    }
+  for (let item of arr) {
+    if (item > maxElement) maxElement = item;
+    if (item < minElement) minElement = item;
+  }
 
-    return [minElement, maxElement];
+  return [minElement, maxElement];
 };
 
 /*
@@ -35,40 +36,42 @@ const findMinMax = (arr: number[]): [number, number] => {
 Problem link: https://www.geeksforgeeks.org/dsa/sum-and-product-of-minimum-and-maximum-element-of-an-array/
 */
 const findSumAndProductOfMinMax = (arr: number[]): [number, number] => {
-    const [minItem, maxItem] = findMinMax(arr);
-    return [minItem + maxItem, minItem * maxItem];
-}
+  const [minItem, maxItem] = findMinMax(arr);
+  return [minItem + maxItem, minItem * maxItem];
+};
 
 /*
 4. Find Minimum steps to make sum and the product of all elements of array non-zero
 Problem link: https://www.geeksforgeeks.org/dsa/minimum-steps-to-make-sum-and-the-product-of-all-elements-of-array-non-zero/
 */
 const stepsToNonZero = (arr: number[]): number => {
-    let steps = 0, total = 0;
-    
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === 0) {
-            steps++;
-            total++;
-        } else steps += arr[i];
-    }
+  let steps = 0,
+    total = 0;
 
-    if (total === 0) steps++;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      steps++;
+      total++;
+    } else steps += arr[i];
+  }
 
-    return steps;
-}
+  if (total === 0) steps++;
+
+  return steps;
+};
 
 /*
 5. Find Minimum steps required to reduce all the elements of the array to zero
 Problem link: https://www.geeksforgeeks.org/dsa/minimum-steps-required-to-reduce-all-the-elements-of-the-array-to-zero/
  */
 const minimumStepsToZero = (arr: number[]): number => {
-    const [_, maxItem] = findMinMax(arr);
-    return maxItem;
-}
+  const [_, maxItem] = findMinMax(arr);
+  return maxItem;
+};
 /*
 6. Sort 0s, 1s and 2s 
 Problem link: https://www.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1
+TODO: Learn from solution
 */
 
 /*
@@ -77,30 +80,30 @@ Problem link: https://www.geeksforgeeks.org/dsa/find-subarray-with-given-sum/
 TODO: Optimize
 */
 const getSubarrayWithSum = (arr: number[], target: number) => {
-    for (let i = 0; i < arr.length; i++) {
-        let subArraySum = arr[i];
-        for (let j = i + 1; j < arr.length; j++) {
-            subArraySum += arr[j];
-            if (subArraySum === target) return [i, j];
-        }
+  for (let i = 0; i < arr.length; i++) {
+    let subArraySum = arr[i];
+    for (let j = i + 1; j < arr.length; j++) {
+      subArraySum += arr[j];
+      if (subArraySum === target) return [i, j];
     }
-    return [-1];
-}
+  }
+  return [-1];
+};
 
 /*
 8. Rotate Array by One
 Problem link: https://www.geeksforgeeks.org/problems/cyclically-rotate-an-array-by-one2614/1
 */
 const rotateArrayByOne = (arr: number[]) => {
-    const n = arr.length;
-    const lastItem = arr[n - 1];
+  const n = arr.length;
+  const lastItem = arr[n - 1];
 
-    for (let i = n - 1; i >= 0; i--) {
-        arr[i] = arr[i - 1];
-    }
+  for (let i = n - 1; i >= 0; i--) {
+    arr[i] = arr[i - 1];
+  }
 
-    arr[0] = lastItem;
-}
+  arr[0] = lastItem;
+};
 
 /*
 9. Count pairs with given sum
@@ -108,13 +111,13 @@ Problem link: https://www.geeksforgeeks.org/problems/count-pairs-with-given-sum-
 TODO: Optimize
 */
 const countPairsForSum = (arr: number[], target: number) => {
-    let total = 0;
+  let total = 0;
 
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = i + 1; j < arr.length; j++) {
-            if ((arr[i] + arr[j] === target)) total++;
-        }
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === target) total++;
     }
+  }
 
-    return total;
-}
+  return total;
+};
--- week-1/strings.ts
+++ week-1/strings.ts
@@ -3,20 +3,21 @@
 Problem link: https://www.geeksforgeeks.org/reverse-words-in-a-given-string/
 */
 const reverseWords = (sentence: string) => {
-    const words = sentence.split(".");
-    let reversedSentence: string[] = [];
+  const words = sentence.split(".");
+  let reversedSentence: string[] = [];
 
-    for (let i = words.length - 1; i >= 0; i--) {
-        const word = words[i];
-        if (word !== "") reversedSentence.push(word);
-    }
+  for (let i = words.length - 1; i >= 0; i--) {
+    const word = words[i];
+    if (word !== "") reversedSentence.push(word);
+  }
 
-    return reversedSentence.join(".");
-}
+  return reversedSentence.join(".");
+};
 
 /*
 2. Longest Common Prefix using Sorting
 Problem link: https://www.geeksforgeeks.org/longest-common-prefix-using-sorting/
+TODO: Learn from solution
 */
 
 /*
@@ -24,39 +25,41 @@ Problem link: https://www.geeksforgeeks.org/longest-common-prefix-using-sorting/
 Problem link: https://www.geeksforgeeks.org/roman-number-to-integer/
 */
 const romanToNumeral = (romanNum: string): number => {
-    const ROMAN: Record<string, number> = {
-        "I": 1,
-        "V": 5,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000
-    }
-    let total = 0;
-    let i = 0;
+  const ROMAN: Record<string, number> = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let total = 0;
+  let i = 0;
 
-    while (i < romanNum.length) {
-        if (ROMAN[romanNum[i]] < ROMAN[romanNum[i + 1]]) {
-            total += ROMAN[romanNum[i + 1]] - ROMAN[romanNum[i]];
-            i += 2;
-        } else {
-            total += ROMAN[romanNum[i]];
-            i++;
-        }
+  while (i < romanNum.length) {
+    if (ROMAN[romanNum[i]] < ROMAN[romanNum[i + 1]]) {
+      total += ROMAN[romanNum[i + 1]] - ROMAN[romanNum[i]];
+      i += 2;
+    } else {
+      total += ROMAN[romanNum[i]];
+      i++;
     }
+  }
 
-    return total;
-}
+  return total;
+};
 
 /*
 4. Converting Decimal Number lying between 1 to 3999 to Roman Numerals
 Problem link: https://www.geeksforgeeks.org/converting-decimal-number-lying-between-1-to-3999-to-roman-numerals/
+TODO: Learn from solution
 */
 
 /*
 5. Check if two given Strings are Isomorphic to each other
 Problem link: https://www.geeksforgeeks.org/check-if-two-given-strings-are-isomorphic-to-each-other/
+TODO: Learn from solution
 */
 
 /*
@@ -64,33 +67,58 @@ Problem link: https://www.geeksforgeeks.org/check-if-two-given-strings-are-isomo
 Problem link: https://www.geeksforgeeks.org/pangram-checking/
 */
 const isPangram = (sentence: string): boolean => {
-    const charFreq: Map<string, number> = new Map();
-    for (let char of sentence) {
-        const lowercaseChar = char.toLowerCase();
-        if (lowercaseChar >= "a" && lowercaseChar <= "z") {
-            charFreq.set(lowercaseChar, charFreq.has(lowercaseChar) ? charFreq.get(lowercaseChar)! + 1 : 1);
-        } 
+  const charFreq: Map<string, number> = new Map();
+  for (let char of sentence) {
+    const lowercaseChar = char.toLowerCase();
+    if (lowercaseChar >= "a" && lowercaseChar <= "z") {
+      charFreq.set(
+        lowercaseChar,
+        charFreq.has(lowercaseChar) ? charFreq.get(lowercaseChar)! + 1 : 1,
+      );
     }
+  }
 
-    return charFreq.size === 26;
-}
+  return charFreq.size === 26;
+};
 
 /*
 7. Count Distinct Subsequences
 Problem link: https://www.geeksforgeeks.org/count-distinct-subsequences/
+TODO: Learn from solution
 */
 
 /*
 8. Program to validate an IP address
 Problem link: https://www.geeksforgeeks.org/program-to-validate-an-ip-address/
 */
+const isValidIpv4 = (address: string): boolean => {
+  const sections = address.split(".");
+
+  if (sections.length !== 4) return false;
+
+  for (let section of sections) {
+    const num = Number(section);
+    if (
+      section === "" ||
+      Number.isNaN(num) ||
+      num < 0 ||
+      num > 255 ||
+      (section.length !== 1 && section[0] === "0")
+    )
+      return false;
+  }
+
+  return true;
+};
 
 /*
 9. Find largest word in dictionary by deleting some characters of given string
 Problem link: https://www.geeksforgeeks.org/find-largest-word-dictionary-deleting-characters-given-string/
+TODO: Learn from solution
 */
 
 /*
 10. Multiply Large Numbers represented as Strings
 Problem link: https://www.geeksforgeeks.org/multiply-large-numbers-represented-as-strings/
+TODO: Learn from solution
 */
